cef8df8fd09547834978c4e8d46d1eb4b200c97e
Pedro Henrique Ribeiro <127356379+class-pedro@users.noreply.github.com>
2026-05-11 23:20:40 -0300

    feat: implement user details endpoint

--- src/main/java/com/example/smart_wallet/controller/AuthenticationController.java
+++ src/main/java/com/example/smart_wallet/controller/AuthenticationController.java
@@ -4,49 +4,51 @@ import com.example.smart_wallet.domain.entity.User;
 import com.example.smart_wallet.dto.AuthenticationDTO;
 import com.example.smart_wallet.dto.CreateUserDTO;
 import com.example.smart_wallet.dto.LoginResponseDTO;
-import com.example.smart_wallet.infrastructure.repository.UserRepository;
-import com.example.smart_wallet.service.AuthenticationServiceImpl;
-import com.example.smart_wallet.service.TokenServiceImpl;
-import com.example.smart_wallet.service.user.UserService;
+import com.example.smart_wallet.dto.MeDTO;
+import com.example.smart_wallet.service.TokenService;
+import com.example.smart_wallet.service.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
 @AllArgsConstructor
 public class AuthenticationController {
 
-    private final AuthenticationServiceImpl authenticationServiceImpl;
     private final AuthenticationManager authenticationManager;
     private final UserService userService;
-    private final TokenServiceImpl tokenServiceImpl;
+    private final TokenService tokenService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody AuthenticationDTO authenticationDTO) {
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody AuthenticationDTO authenticationDTO) {
         UsernamePasswordAuthenticationToken usernamePassword = new UsernamePasswordAuthenticationToken(
                 authenticationDTO.getEmail(),
                 authenticationDTO.getPassword()
         );
 
         Authentication authentication = authenticationManager.authenticate(usernamePassword);
-
-        String token = tokenServiceImpl.generateToken((User) authentication.getPrincipal());
+        String token = tokenService.generateToken((User) authentication.getPrincipal());
 
         return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
     @PostMapping("/sign-up")
-    public ResponseEntity<?> signUp(@RequestBody @Valid CreateUserDTO createUserDTO) {
-        userService.createUser(createUserDTO);
+    public ResponseEntity<String> signUp(@RequestBody @Valid CreateUserDTO createUserDTO) {
+        userService.signUpUser(createUserDTO);
 
-        return ResponseEntity.ok("Usuário criado");
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/me")
+    public ResponseEntity<MeDTO> me(@RequestHeader("Authorization") String authHeader) {
+        String walletId = tokenService.getWalletIdFromToken(authHeader);
+
+        return ResponseEntity.ok(new MeDTO(walletId));
     }
 }
--- src/main/java/com/example/smart_wallet/dto/MeDTO.java
+++ src/main/java/com/example/smart_wallet/dto/MeDTO.java
@@ -1,4 +1,4 @@
 package com.example.smart_wallet.dto;
 
-public record MeDTO() {
+public record MeDTO(String walletId) {
 }
