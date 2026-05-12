9792c64760a121122417c3624a84370744a631d2
Pedro Henrique Ribeiro <127356379+class-pedro@users.noreply.github.com>
2026-05-11 23:16:13 -0300

    refactor: adjust CardController returns to use ResponseEntity

--- src/main/java/com/example/smart_wallet/controller/CardController.java
+++ src/main/java/com/example/smart_wallet/controller/CardController.java
@@ -2,12 +2,13 @@ package com.example.smart_wallet.controller;
 
 import com.example.smart_wallet.dto.CreateCardDTO;
 import com.example.smart_wallet.dto.GetCardIdAndNameDTO;
-import com.example.smart_wallet.service.card.CardService;
+import com.example.smart_wallet.service.CardService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/cards")
@@ -16,12 +17,13 @@ public class CardController {
     private final CardService cardService;
 
     @PostMapping
-    public void createCard(@RequestBody CreateCardDTO cardDTO) {
+    public ResponseEntity<String> createCard(@RequestBody CreateCardDTO cardDTO) {
         cardService.create(cardDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/cards-to-input")
-    public List<GetCardIdAndNameDTO> getCardIdAndNameById(@RequestParam String walletId) {
-        return cardService.getCardIdAndNameById(walletId);
+    public ResponseEntity<List<GetCardIdAndNameDTO>> getCardIdAndNameById(@RequestParam String walletId) {
+        return ResponseEntity.ok(cardService.getCardIdAndNameById(walletId));
     }
 }
